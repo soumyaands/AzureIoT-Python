6a83fee13bd92340e397d4a03302da92650eb12c
soumyaands <106863981+soumyaands@users.noreply.github.com>
2022-06-18 11:49:08 +0000

    Rectification

---
+++ AzureMain.py
@@ -0,0 +1,5 @@
+import DeviceCreation as DC
+#DC.addDevice("Test",10)
+DC.getDevice(10)
+# for i in range(0, 10):
+#     DC.deleteDevice("device_Test_"+str(i))
--- DeviceCreation.py
+++ DeviceCreation.py
@@ -1,19 +1,22 @@
 import base64
 import os
+import uuid
 from azure.iot.hub import IoTHubRegistryManager
 from azure.iot.hub.models import SymmetricKey, AuthenticationMechanism, ExportImportDevice
-
-iothub_connection_string = os.getenv("IOTHUB_CONNECTION_STRING")
-iothub_registry_manager = IoTHubRegistryManager.from_connection_string(iothub_connection_string)
+from Utility import Utilities
+iothub_connection_string = "HostName=Safiya-IoT-Hub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=dWEhcgJxlIQoav2EjWOKSWJNpt403bCzMfsVAfTNey0=";
+#os.getenv("IOTHUB_CONNECTION_STRING")
+global iothub_registry_manager #= IoTHubRegistryManager.from_connection_string(iothub_connection_string)
 def addDevice(deviceFamily,numberOfDevices):
     deviceList = []
+    iothub_registry_manager = IoTHubRegistryManager.from_connection_string(iothub_connection_string)
     try:
         for i in range(0,numberOfDevices):
             primaryKey = base64.b64encode(str(uuid.uuid4()).encode()).decode()
             secondaryKey = base64.b64encode(str(uuid.uuid4()).encode()).decode()
             symmetricKey = SymmetricKey(primary_key=primaryKey,secondary_key=secondaryKey)
             authentication = AuthenticationMechanism(type="sas",symmetric_key=symmetricKey)
-            device = ExportImportDevice(id="device"+i,status="enabled",authentication=authentication)
+            device = ExportImportDevice(id="device_"+deviceFamily+"_"+str(i),status="enabled",authentication=authentication)
 
             #DEVICE_CREATION
             device.import_mode="create"
@@ -25,12 +28,13 @@ def addDevice(deviceFamily,numberOfDevices):
     return deviceList
 
 def getDevice(numberOfDevices):
+    iothub_registry_manager = IoTHubRegistryManager.from_connection_string(iothub_connection_string)
     try:
         deviceList = iothub_registry_manager.get_devices(numberOfDevices)
         if deviceList:
             devicePos = 0
             for device in deviceList:
-                printDeviceInfo("{0}th device".format(devicePos),device)
+                Utilities.printDeviceInfo("{0}th device".format(devicePos),device)
                 devicePos+=1
         else:
             print("No Devices Found")
@@ -38,10 +42,11 @@ def getDevice(numberOfDevices):
         print("Some Exception Description : {0}".format(e))
     
 
-def deleteDevice(deviceName):
-    try:
-        device = ExportImportDevice(id=deviceName,status="enabled",authentication=authentication)
-        device.import_mode = "delete"
-        iothub_registry_manager.bulk_create_or_update_devices(device)
-    except Exception as e:
-        print("Some Exception Description : {0}".format(e))
+# def deleteDevice(deviceName):
+#     try:
+#         authentication = AuthenticationMechanism(type="sas",symmetric_key=symmetricKey)
+#         device = ExportImportDevice(id=deviceName,status="enabled",authentication=authentication)
+#         device.import_mode = "delete"
+#         iothub_registry_manager.bulk_create_or_update_devices(device)
+#     except Exception as e:
+#         print("Some Exception Description : {0}".format(e))
