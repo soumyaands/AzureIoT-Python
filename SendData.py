a0ba5cd4e70f699085c4a7f7ab75a9727e7385ee
soumyaands <106863981+soumyaands@users.noreply.github.com>
2022-06-24 06:31:44 +0000

    Version2

--- SendData.py
+++ SendData.py
@@ -4,9 +4,10 @@ from DeviceCreation import getDevice
 import threading
 import time
 import random
+import multiprocessing
 from azure.iot.device import IoTHubDeviceClient
 from datetime import datetime
-iothub_connection_string = "HostName=Safiya-IoT-Hub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=dWEhcgJxlIQoav2EjWOKSWJNpt403bCzMfsVAfTNey0=";
+iothub_connection_string = "HostName=Safiya-IoT-Hub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=w31yH21GCoNQz6/LBoKtS01iL3qvp0jEXUiwYcRSrHA=";
 def sendDataToDevice(numberOfDevices,numberOfRequests):
     HostName = "Safiya-IoT-Hub.azure-devices.net"
     ihrm = IHRM.from_connection_string(iothub_connection_string)
@@ -25,14 +26,16 @@ def sendDataToDevice(numberOfDevices,numberOfRequests):
                 messageToSend = "{'time':'"+str(dateTimeNow)+"','data':'"+str(random.randint(1,10))+"'}"
                 print(messageToSend)
                 iothubdeviceclient = IoTHubDeviceClient.create_from_connection_string(deviceConnectionString)
-                #iothubdeviceclient.send_message(messageToSend)
-                threading.Thread(target=iothubdeviceclient.send_message,args=(messageToSend))
-                # ihrm.send_c2d_message(deviceId,messageToSend)
-                # threading.Thread(target = ihrm.send_c2d_message, args=(deviceId,messageToSend))
-                # # IHRM.sendMessage(messageToSend)
+                iothubdeviceclient.connect()
+                iothubdeviceclient.send_message(messageToSend)
+                
+            #threading.Thread(target=iothubdeviceclient.send_message,args=(messageToSend))
+            # ihrm.send_c2d_message(deviceId,messageToSend)
+            # threading.Thread(target = ihrm.send_c2d_message, args=(deviceId,messageToSend))
+            # # IHRM.sendMessage(messageToSend)
     # except Exception as e:
     #     print(e)
-
+mutex = threading.Lock()                                                   
 def split_processing(numberOfDevices,numberOfRequests, num_splits=4):                                      
     split_size = 2#len(items) // num_splits                                       
     threads = []                                                                
@@ -41,10 +44,15 @@ def split_processing(numberOfDevices,numberOfRequests, num_splits=4):
         start = i * split_size                                                  
         # special case on the last chunk to account for uneven splits           
         end = None if i+1 == num_splits else (i+1) * split_size                 
-        # create the thread                                                     
-        threads.append(                                                         
-            threading.Thread(target=sendDataToDevice, args=(numberOfDevices,numberOfRequests)))         
-        threads[-1].start() # start the thread we just created                  
+        # create the thread 
+        #mutex.acquire()
+        process = multiprocessing.Process(target = sendDataToDevice,args=(numberOfDevices,numberOfRequests))
+        print(process.is_alive())
+        process.start()
+        # threads.append(                                                         
+        #     threading.Thread(target=sendDataToDevice, args=(numberOfDevices,numberOfRequests)))         
+        # threads[-1].start() # start the thread we just created                  
+        #mutex.release()
 
     # wait for all threads to finish                                            
     for t in threads:                                                           
